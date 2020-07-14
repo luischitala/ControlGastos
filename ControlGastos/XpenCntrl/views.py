23a1590461eeee6a506537c55b23ec8d942cb795
Luis Chitala <53163045+luischitala@users.noreply.github.com>
2020-07-14 18:55:17 -0500

    login required successfully added

--- ControlGastos/XpenCntrl/views.py
+++ ControlGastos/XpenCntrl/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from XpenCntrl.models import Category 
 from django.views.generic import ListView
+from django.utils.decorators import method_decorator
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
@@ -18,6 +20,14 @@ def category_list(request):
 class CategoryListView(ListView):
     model = Category
     template = '/categoria/list.html'
+
+    @method_decorator(login_required)
+    def dispatch(self, request, *args, **kwargs):
+        return super().dispatch(request, *args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+        data = {'name':'Luis'}
+        return JsonResponse(data)
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
