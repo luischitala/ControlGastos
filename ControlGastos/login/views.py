0542bb50099b4de31ab4dff4f4327bbf5c257ca0
Luis Chitala <53163045+luischitala@users.noreply.github.com>
2020-07-14 09:09:09 -0500

    Redirect added along with verification

--- ControlGastos/login/views.py
+++ ControlGastos/login/views.py
@@ -1,10 +1,16 @@
 from django.contrib.auth.views import LoginView, FormView
+from django.shortcuts import redirect
 
 
 class LoginFormView(LoginView):
     template_name = 'login.html'
 
+    def dispatch(self, request, *args, **kwargs):
+        if request.user.is_authenticated:
+            return redirect('/admin/')
+        return super().dispatch(request, *args, **kwargs)
+
     def get_context_data(self, **kwargs):
-        context = super().get_context_data(*kwargs)
+        context = super().get_context_data(**kwargs)
         context['title'] = 'Iniciar Sesión'
         return context
